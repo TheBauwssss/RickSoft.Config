f836a42d18f08ce34bbd6ecb4114e210e9cb5cc9
TheBauwssss <github@rick-soft.com>
2017-02-06 16:57:49 +0100

    Added test config project with example

---
+++ ExampleApplication/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using RickSoft.Config;
+
+namespace ExampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetupConsoleLogging();
+
+            TestConfig cfg = JsonConfig.Load<TestConfig>("test_config");
+
+            Console.WriteLine(cfg.Name);
+            Console.WriteLine(cfg.Test);
+
+            cfg.Test = "test2";
+            cfg.Save();
+
+            Console.WriteLine(cfg.Name);
+            Console.WriteLine(cfg.Test);
+
+            Console.ReadLine();
+
+            cfg.Reload();
+        }
+
+        static void SetupConsoleLogging()
+        {
+            var t = new ColoredConsoleTarget();
+            
+	        var r = new LoggingRule("*", LogLevel.Trace, t);
+	        LogManager.Configuration = new LoggingConfiguration();
+	        LogManager.Configuration.AddTarget("console", t);
+	        LogManager.Configuration.LoggingRules.Add(r);
+
+	        LogManager.ReconfigExistingLoggers();
+        }
+    }
+}
---
+++ ExampleApplication/TestConfig.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using RickSoft.Config;
+
+namespace ExampleApplication
+{
+    public class TestConfig : JsonConfig
+    {
+        [JsonProperty("Test")]
+        public string Test { get; set; } = "default value";
+
+        [JsonProperty("Name")]
+        public string Name { get; set; } = "Jan";
+    }
+}
